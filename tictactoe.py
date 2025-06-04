8adac240cb69968f280bac6ca78ea36d7ea34821
Edyth-K <kelvin.dcruz@gmail.com>
2025-06-04 03:36:03 -0500

    updated tictactoe.py to pygame implementation

--- tictactoe.py
+++ tictactoe.py
@@ -1,71 +1,132 @@
-# game logic and rendering
+import sys
+import struct
+import threading
+import socket
+import pygame
 
-def render_board(board):
-    """
-    Draw board to screen
+class TicTacToe:
+    def __init__(self, host='127.0.0.1', port=9999):
+
+        self.host = host
+        self.port = port
+
+        self.socket = None
+
+        self.null_char = 'n'
+        self.board = [self.null_char] * 9
+
+        self.turn = 0
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 850))
+        self.clock = pygame.time.Clock()
+
+        self.space_size = 250
+        self.board_center = (self.screen.get_width() / 2, self.screen.get_height() - self.screen.get_width() / 2)
+        self.winner = None
+        self.teams = ['x', 'o']
+
+        self.clicked = False
+
+        self.font = pygame.font.Font(size=34)
+
+    def draw_letter(self, letter, pos):
+        if letter == self.teams[0]:
+            pygame.draw.line(self.screen, (0, 0, 255),
+                             (pos[0] - self.space_size * 0.4, pos[1] - self.space_size * 0.4),
+                             (pos[0] + self.space_size * 0.4, pos[1] + self.space_size * 0.4), 10)
+            pygame.draw.line(self.screen, (0, 0, 255),
+                             (pos[0] + self.space_size * 0.4, pos[1] - self.space_size * 0.4),
+                             (pos[0] - self.space_size * 0.4, pos[1] + self.space_size * 0.4), 10)
+        if letter == self.teams[1]:
+            pygame.draw.circle(self.screen, (255, 0, 0), pos, self.space_size * 0.4, 10)
+
+    def draw_board(self):
+        pygame.draw.line(self.screen, (0, 0, 0),
+                         (self.board_center[0] - self.space_size / 2, self.board_center[1] - self.space_size * 1.5),
+                         (self.board_center[0] - self.space_size / 2, self.board_center[1] + self.space_size * 1.5))
+        pygame.draw.line(self.screen, (0, 0, 0),
+                         (self.board_center[0] + self.space_size / 2, self.board_center[1] - self.space_size * 1.5),
+                         (self.board_center[0] + self.space_size / 2, self.board_center[1] + self.space_size * 1.5))
+        pygame.draw.line(self.screen, (0, 0, 0),
+                         (self.board_center[0] - self.space_size * 1.5, self.board_center[1] - self.space_size / 2),
+                         (self.board_center[0] + self.space_size * 1.5, self.board_center[1] - self.space_size / 2))
+        pygame.draw.line(self.screen, (0, 0, 0),
+                         (self.board_center[0] - self.space_size * 1.5, self.board_center[1] + self.space_size / 2),
+                         (self.board_center[0] + self.space_size * 1.5, self.board_center[1] + self.space_size / 2))
+        
+    def place(self, space_index):
+        if 0 <= space_index < len(self.board):
+            if self.board[space_index] == self.null_char:
+                self.board[space_index] = self.teams[self.turn % 2]
+                self.turn += 1
+
+    def get_space(self, pos):
+        space_index = pos[0] + pos[1] * 3
+        if (0 <= pos[0] < 3) and (0 <= pos[1] < 3):
+            return self.board[space_index]
+
+    def check_win(self):
+        for i, letter in enumerate(self.board):
+            if letter != self.null_char:
+                space_id = (i % 3, i // 3)
+                for angle in [(1, 0), (1, 1), (0, 1), (-1, 1)]:
+                    for j in range(3):
+                        if self.get_space((space_id[0] + angle[0] * j, space_id[1] + angle[1] * j)) != letter:
+                            break
+                        if j == 2:
+                            return letter
     
-    Args:
-        board (list): 3x3 list of lists representing the game board
-    """
+    def update(self):
+        self.screen.fill((255, 255, 255))
 
-    print ("-------------")
-    for row in board:
-        print (f"| {row[0]} | {row[1]} | {row[2]} |")
-        print ("-------------")
+        self.clicked = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.clicked = True
+        
+        mouse_pos = pygame.mouse.get_pos()
 
-def update_board(board, pos, player):
-    """
-    Update board state
-    
-    Args:
-        board (list): 3x3 list of lists representing the game board
-        pos (int): (1-9) representing where to place o/x
-        player (string): "o" or "x" to indicate which to draw
+        game_text = f'{self.teams[self.turn % 2]}\'s turn'
+        if self.winner:
+            game_text = f'{self.winner} wins!'
+        self.screen.blit(self.font.render(game_text, True, (0, 0, 0)), (10, 10))
 
-    Returns: 
-        board (list): 3x3 list of lists representing updated board
-    """
-    match pos:
-        case 1:
-            board[0][0] = player
-        case 2:
-            board[0][1] = player
-        case 3:
-            board[0][2] = player
-        case 4:
-            board[1][0] = player
-        case 5:
-            board[1][1] = player
-        case 6:
-            board[1][2] = player
-        case 7:
-            board[2][0] = player
-        case 8:
-            board[2][1] = player
-        case 9:
-            board[2][2] = player
-        case _:
-            return board
-    return board
+        self.draw_board()
+        for i, letter in enumerate(self.board):
+            space_id = (i % 3, i // 3)
+            space_center = ((space_id[0] - 1) * self.space_size + self.board_center[0], (space_id[1] - 1) * self.space_size + self.board_center[1])
+            space_r = pygame.Rect(space_center[0] - self.space_size / 2, space_center[1] - self.space_size / 2, self.space_size, self.space_size)
+            if letter != self.null_char:
+                self.draw_letter(letter, space_center)
+            if not self.winner:
+                if space_r.collidepoint(mouse_pos):
+                    if self.clicked:
+                        self.place(i)
+        
+        self.winner = self.check_win()
+            
+        pygame.display.update()
 
+        self.clock.tick()
 
-# remove later
-def main():
-    board = [
-    [' ',' ',' '],
-    [' ',' ',' '],
-    [' ',' ',' ']
-]
-    
-    print("Welcome to Tic Tac Toe.")
-    render_board(board)
+    def run_listener(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+            s.connect((self.host, self.port))
+            s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True) # lets all packets be processed asap, by default TCP bundles packets before processing
+            s.settimeout(1)
+            print("Connected: ", s)
+            self.socket = s
+            
 
-    # Game Loop
-    while True:
-        input_position = input("Enter Position (1-9): ")
-        board = update_board(board, int(input_position), "x")
-        print(board)
-        render_board(board)
+    def run(self):
+        threading.Thread(target=self.run_listener).start()
+        while True:
+            self.update()
 
-if __name__ == "__main__":
-    main()
+TicTacToe().run()
